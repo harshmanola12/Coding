61f88f9c17f406753693c54d0508c0e02b09b303
harshmanola12 <hsharshsingh1234@gmail.com>
2023-11-24 23:57:14 +0530

    First Commit

--- src/dsa/questions/CountDigit.java
+++ src/dsa/questions/CountDigit.java
@@ -1,15 +1,27 @@
 package dsa.questions;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CountDigit {
 	public static void main(String[] args) {
 
-		try (Scanner sc = new Scanner(System.in)) {
-			int number = sc.nextInt();
-			int h =countDigit(number);
-			System.out.println("Digit  "+h);
-		}
+//		try (Scanner sc = new Scanner(System.in)) {
+//			int number = sc.nextInt();
+//			int h =countDigit(number);
+//			System.out.println("Digit  "+h);
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		finally{
+//			System.exit(0);
+//			System.gc();
+//		}
+//		System.exit(0);
+//		System.out.println("Hello");
+		sumMapValue();
 	}
 
 	private static int countDigit(int n) {
@@ -19,5 +31,25 @@ public class CountDigit {
 			i++;
 		}
 		return i;
+	}
+	private static void sumMapValue() {
+		HashMap<Integer,Integer> hp = new HashMap();
+		hp.put(1,78);
+		hp.put(2,77);
+		hp.put(1,87);
+		hp.put(2,67);
+		hp.put(3,76);
+		System.out.println(hp);
+//		Set<Entry<Integer, Integer>> g = hp.entrySet();
+//		int index =0;
+//		for(Entry<Integer, Integer> h :g) {
+//			int k = h.getKey();
+//			int v = h.getValue();
+//			
+//			System.out.println(k);
+//			System.out.println(v);
+//		}
+		
+		
 	}
 }
